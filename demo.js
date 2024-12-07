998b6baedbf156508c9f8a5e35d614e6ec73607c
bharanikumar843 <95572380+bharanikumar843@users.noreply.github.com>
2024-12-07 13:09:22 +0530

    Add files via upload

---
+++ demo.js
@@ -0,0 +1,85 @@
+
+var alphabet=(prompt("Entet the Alphabet"));
+
+switch(alphabet)
+{
+    case "A":document.write("Apple");
+    break;
+
+    case "B":document.write("Bus");
+    break;
+
+    case "C":document.write("Car");
+    break;
+
+    case "D":document.write("Dear");
+    break;
+
+    case "E":document.write("Entri");
+    break;
+
+    case "F":document.write("fruit");
+    break;
+
+    case "G":document.write("Goat");
+    break;
+
+    case "H":document.write("happy");
+    break;
+
+    case "I":document.write("ice");
+    break;
+
+    case "J":document.write("jam");
+    break;
+
+    case "K":document.write("killer");
+    break;
+
+    case "L":document.write("Law");
+    break;
+
+    case "M":document.write("Monday");
+    break;
+
+    case "N":document.write("Netflix");
+    break;
+
+    case "O":document.write("Onion");
+    break;
+
+    case "P":document.write("police");
+    break;
+
+    case "Q":document.write("Question");
+    break;
+
+    case "R":document.write("Rocket");
+    break;
+
+    case "S":document.write("Sun");
+    break;
+
+    case "T":document.write("Tiger");
+    break;
+
+    case "U":document.write("Undefind");
+    break;
+
+    case "V":document.write("Van");
+    break;
+
+    case "W":document.write("Watch");
+    break;
+
+    case "X":document.write("Xerox");
+    break;
+
+    case "Y":document.write("Yellow");
+    break;
+
+    case "Z":document.write("Zoo");
+    break;
+    default:document.write("No Data Found");
+
+}
